124f10795b38a00a62315d8b7ccfb0f39e3ccfa1
Алексей Тен <virtuoso_ten@mail.ru>
2023-06-27 22:10:06 +0300

    Работает все на удаленном сервере БЕЗ картинок

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.201.153.152', '127.0.0.1', 'localhost', 'aten-kittygramm.sytes.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
