e938c0461c59d8a316584347665a0fb1b933b0b3
Алексей Тен <virtuoso_ten@mail.ru>
2023-06-28 14:05:05 +0300

    Added tests check and fixed settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,8 +1,11 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
