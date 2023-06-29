b117010bc69e46e1c8f11131906361663ca23f0a
Алексей Тен <virtuoso_ten@mail.ru>
2023-06-29 22:45:07 +0300

    Created Readme

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -60,8 +60,8 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
-        'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
+        'NAME': os.getenv('POSTGRES_DB', ''),
+        'USER': os.getenv('POSTGRES_USER', ''),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
