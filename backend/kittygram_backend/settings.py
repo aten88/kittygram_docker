3eb3ba86ca0f97bbff069f478896e58601acb22b
Алексей Тен <virtuoso_ten@mail.ru>
2023-06-29 22:02:05 +0300

    Return Database in settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -60,11 +60,11 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', ''),
-        'USER': os.getenv('POSTGRES_USER', ''),
+        'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
+        'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', '')
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
